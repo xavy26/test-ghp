a2b0dd9ccc3e30ea3e25f8fa9c35df05f8de6fcb
xavy26 <luis.x.paredes@unl.edu.ec>
2021-08-01 22:38:47 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.parallax').parallax();
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, options);
+});
